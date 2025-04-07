1cfb527fbf656a5f3cace8169fae30236a8ae51a
kimia <doulabikimia@gmail.com>
2025-04-07 17:37:17 -0400

    Q15

--- CommandWords.java
+++ CommandWords.java
@@ -13,7 +13,8 @@ public class CommandWords
 {
     //// a constant array that holds all valid command words
     private static final String[] validCommands = {
-        "go", "quit", "help"
+    //Q15
+    "go", "quit", "help", "Look"
     };
 
     /**
